618ff33bdc46a1e369edcae4b50c7f9661c0b72c
Michael Kilong <michaelkilongz@gmail.com>
2026-06-22 17:35:54 +0530

    Update InputArea.tsx

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -229,7 +229,7 @@ export default function InputArea({ onSend, onStop, isSending, isStreaming, dail
                   <line x1="2" y1="12" x2="22" y2="12"/>
                   <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/>
                 </svg>
-                <span>Search Web</span>
+                <span>Search</span>
               </button>
             </div>
 
